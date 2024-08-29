65de79d9acfb48fa37b731ab10267cc839a85beb
Rustam Rakhmatullov <rahmatullovri@gmail.com>
2024-08-29 16:20:22 +0400

    lesson6

---
+++ chapter1/lesson6/ex1.10/ex1.10.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+		// Получение из канала ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // Отправка в канал ch
+		return
+	}
+	file, err := os.Create("dump.html")
+	if err != nil {
+		ch <- err.Error()
+	}
+	nbytes, err := io.Copy(file, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
